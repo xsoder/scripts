94f81e8932330e5a9b955befa2ccc63be43a35cf
csodelinux <csodelinux@gmail.com>
2025-04-24 18:47:50 -0400

    updated tmux sessionizer

--- tmux_sessionizer.sh
+++ tmux_sessionizer.sh
@@ -1,48 +1,37 @@
 #!/usr/bin/env bash
-switch_to() {
-    if [[ -z $TMUX ]]; then
-        tmux attach-session -t $1
-    else
-        tmux switch-client -t $1
-    fi
-}
 
 has_session() {
-    tmux list-sessions | grep -q "^$1:"
+    tmux has-session -t "$1" 2>/dev/null
 }
 
 hydrate() {
-    if [ -f $2/.tmux-sessionizer ]; then
-        tmux send-keys -t $1 "source $2/.tmux-sessionizer" c-M
-    elif [ -f $HOME/.tmux-sessionizer ]; then
-        tmux send-keys -t $1 "source $HOME/.tmux-sessionizer" c-M
+    if [ -f "$2/.tmux-sessionizer" ]; then
+        tmux send-keys -t "$1:1" "source $2/.tmux-sessionizer" C-m
+    elif [ -f "$HOME/.tmux-sessionizer" ]; then
+        tmux send-keys -t "$1:1" "source $HOME/.tmux-sessionizer" C-m
     fi
 }
 
 if [[ $# -eq 1 ]]; then
-    selected=$1
+    selected="$1"
 else
-    # If someone wants to make this extensible, i'll accept
-    # PR
-    selected=$(find ~/ ~/setup/ ~/dopi ~/Programming/ ~/DEV  -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/ ~/setup/ ~/dopi ~/Programming/ ~/DEV -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
-if [[ -z $selected ]]; then
-    exit 0
-fi
+[[ -z $selected ]] && exit 0
 
 selected_name=$(basename "$selected" | tr . _)
-tmux_running=$(pgrep tmux)
 
-if [[ -z $TMUX ]] && [[ -z $tmux_running ]]; then
-    tmux new-session -s $selected_name -c $selected
-    hydrate $selected_name $selected
-    exit 0
+if ! has_session "$selected_name"; then
+    tmux new-session -s "$selected_name" -c "$selected" -n "nvim" -d "nvim"
+    tmux new-window -t "$selected_name:" -n "shell" -c "$selected"
+    hydrate "$selected_name" "$selected"
 fi
 
-if ! has_session $selected_name; then
-    tmux new-session -ds $selected_name -c $selected
-    hydrate $selected_name $selected
+if [[ -z $TMUX ]]; then
+    tmux attach-session -t "$selected_name" \; select-window -t "$selected_name:0"
+else
+    tmux switch-client -t "$selected_name"
+    tmux select-window -t "$selected_name:0"
 fi
 
-switch_to $selected_name
