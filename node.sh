f913eac6a59dee7e677e39892c05b57a20976e03
xsoder <tadihailukebe@gmail.com>
2025-08-06 23:28:09 -0400

    New changes

--- node.sh
+++ node.sh
@@ -1,16 +1,10 @@
 # Download and install nvm:
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
-# in lieu of restarting the shell
 \. "$HOME/.nvm/nvm.sh"
 
-# Download and install Node.js:
 nvm install 23
 
-# Verify the Node.js version:
-node -v # Should print "v23.11.0".
-nvm current # Should print "v23.11.0".
-
-# Verify npm version:
-npm -v # Should print "10.9.2".
-
+node -v 
+nvm current
+npm -v
--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-WALLPAPER_DIR="/home/csode/devenv/wallpaper/gruvbox"
-STATE_FILE="/home/csode/.cache/current_wallpaper.txt"
+WALLPAPER_DIR="/home/xsoder/wallpaper/penger"
+STATE_FILE="/home/xsoder/.cache/current_wallpaper.txt"
 
 mapfile -t WALLPAPERS < <(find "$WALLPAPER_DIR" -type f \( -iname "*.jpg" -o -iname "*.png" -o -iname "*.jpeg" \) | sort)
 if [ ${#WALLPAPERS[@]} -eq 0 ]; then
@@ -9,7 +9,9 @@ if [ ${#WALLPAPERS[@]} -eq 0 ]; then
     exit 1
 fi
 
+mkdir -p "$(dirname "$STATE_FILE")"
 LAST_WALLPAPER=$(cat "$STATE_FILE" 2>/dev/null)
+
 INDEX=0
 for i in "${!WALLPAPERS[@]}"; do
     if [[ "${WALLPAPERS[$i]}" == "$LAST_WALLPAPER" ]]; then
@@ -19,6 +21,6 @@ for i in "${!WALLPAPERS[@]}"; do
 done
 
 NEXT_WALLPAPER="${WALLPAPERS[$INDEX]}"
-swww img "$NEXT_WALLPAPER" --transition-type any --transition-duration 1.0
+feh --bg-fill "$NEXT_WALLPAPER"
 echo "$NEXT_WALLPAPER" > "$STATE_FILE"
 notify-send "Wallpaper Changed" "Set to: $(basename "$NEXT_WALLPAPER")"
